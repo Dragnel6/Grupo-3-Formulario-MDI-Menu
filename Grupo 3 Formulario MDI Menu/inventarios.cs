f33baa9cc6577c6209cd18058d58f3b3f042e000
Carlos Contreras <cdcontreras43@gmail.com>
2021-08-29 16:10:57 -0600

    Agregue Inventarios Carlos Contreras

---
+++ Grupo 3 Formulario MDI Menu/inventarios.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grupo_3_Formulario_MDI_Menu
+{
+    public partial class inventarios : Form
+    {
+        public inventarios()
+        {
+            inventarios f = new Ventas();
+            f.MdiParent = this;
+            f.Text = "inventarios - " + this.MdiChildren.Length.ToString();
+            f.Show();
+            InitializeComponent();
+        }
+
+        private void inventarios_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
